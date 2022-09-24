62e00e13d8c822d3639c10751e2368965881e579
TrustedInstaller <trustedinster@outlook.com>
2022-09-24 04:01:47 +0000

    删除文件 t_index.php

--- t_index.php
+++
@@ -1,9 +0,0 @@
-<?php
-// 返回二进制流数据
-$file_path = __DIR__ . '/Horror_MUSIC.mp3';
-$file_size = filesize($file_path);
-$oct_data = fread(fopen($file_path, "r"), $filesize);
- 
-header("Content-type: video/mpeg4;charset=UTF-8");
-header("Content-Length: " . $filesize);
-echo $oct_data;
